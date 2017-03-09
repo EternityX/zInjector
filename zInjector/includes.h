2c8d4a2314ad5272005e23d0f96da14c0c80819d
Eternity <jeramie@protonmail.com>
2017-03-08 23:33:30 -0800

    Remove unused headers

--- zInjector/includes.h
+++ zInjector/includes.h
@@ -4,15 +4,10 @@
 #pragma once  
 
 #include <windows.h>
-#include <assert.h>
 #include <tlhelp32.h>
 #include <iostream>
-#include <excpt.h>
-#include <signal.h>
 #include <shlwapi.h>
 #include <string>
-#include <fstream>
-#include <experimental/filesystem>
 #include "utilities.h"
 
 #pragma comment (lib, "Shlwapi.lib")
